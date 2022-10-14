0bfdb11cc4633af02bf017c1b06b98f82582a278
GeorgeOfGithub <lucasbuhelt@gmail.com>
2022-10-14 12:01:14 +0200

    module7

---
+++ Module7.py
@@ -0,0 +1,22 @@
+import numpy as np
+import pandas as pd
+
+
+def textToNato(plainText):
+    to_print = np.zeros(0)
+    converter =  {
+        'a': 'Alpha',  'b': 'Bravo',   'c': 'Charlie',
+        'd': 'Delta',  'e': 'Echo',    'f': 'Foxtrot',
+        'g': 'Golf',   'h': 'Hotel',   'i': 'India',
+        'j': 'Juliett','k': 'Kilo',    'l': 'Lima',
+        'm': 'Mike',   'n': 'November','o': 'Oscar',
+        'p': 'Papa',   'q': 'Quebec',  'r': 'Romeo',
+        's': 'Sierra', 't': 'Tango',   'u': 'Uniform',
+        'v': 'Victor', 'w': 'Whiskey', 'x': 'X-ray',
+        'y': 'Yankee', 'z': 'Zulu'}
+    for i in range(len(plainText)):
+        to_print = np.append(to_print, converter[plainText[i]])
+    printing = str.join("-",to_print)
+    return printing
+
+print(textToNato("hello"))
